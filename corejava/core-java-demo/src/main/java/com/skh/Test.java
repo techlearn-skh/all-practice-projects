302cafce34d2aac6a9812e637566852aa59e7f25
kamal <skkamal2java@gmail.com>
2026-06-09 22:56:01 +0530

    new changes

--- corejava/core-java-demo/src/main/java/com/skh/Test.java
+++ corejava/core-java-demo/src/main/java/com/skh/Test.java
@@ -5,8 +5,10 @@ import com.skh.interfaces.AnimalInterface;
 
 public class Test {
     public static void main(String[] args) {
+    // void  m1(int age, String village);
 
-
+        AnimalInterface ref = (int age, String village) -> System.out.println("asasasas");
+        ref.m1(1,"asasas");
 
 
     }
