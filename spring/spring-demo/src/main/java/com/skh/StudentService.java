d4ce6b42dfc50236fcd637e666ababbc2589cdfa
kamal <skkamal2java@gmail.com>
2026-06-28 07:48:29 +0530

    new changes

---
+++ spring/spring-demo/src/main/java/com/skh/StudentService.java
@@ -0,0 +1,10 @@
+package com.skh;
+
+public class StudentService {
+    public void printMyDetails(){
+        System.out.println("Hi this is Azad....");
+    }
+    public void m1(){
+        System.out.println("Hi this is kamal....");
+    }
+}
